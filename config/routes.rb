7e711bfbe779ef057772cb63fa848658976f2bb1
song <afsd0104@google.com>
2025-04-10 12:10:36 +0900

    login&logout / post new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "home/top"
+  # get "posts/new"
+  # get "home/top"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -13,4 +14,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root "home#top"
+  resources :posts, only: [ :new, :create ]
 end
--- db/migrate/20250409141020_create_posts.rb
+++ db/migrate/20250409141020_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[8.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :content
+      t.string :title, null: false
+      t.string :content, null: false
+      t.references :user, foreign_key: true, null: false # 外部キー (User)
       t.timestamps
     end
   end
